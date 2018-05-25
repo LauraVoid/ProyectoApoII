626cb067f396e1a25a3b13734eda96ae628a9c1c
Laura <Laura@LAPTOP-UD3NP6G9>
2018-05-25 14:34:43 -0500

    Pruebas

---
+++ Proyecto/src/pruebas/ZumaTest.java
@@ -0,0 +1,16 @@
+package pruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ZumaTest {
+	
+	
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
