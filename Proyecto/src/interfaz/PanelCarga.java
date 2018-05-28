2a172c617f4bc12d33d4645052eaba3cc6cba66a
Laura <Laura@LAPTOP-UD3NP6G9>
2018-05-28 00:13:53 -0500

    FAJARDO PRESIDENTE

--- Proyecto/src/interfaz/PanelCarga.java
+++ Proyecto/src/interfaz/PanelCarga.java
@@ -8,53 +8,46 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class PanelCarga extends JPanel {
-	
+
 	private ImageIcon imagen;
-	
+
 	public PanelCarga() {
 		setSize(700, 515);
-		imagen= new ImageIcon("./imagenes/fondo-negro.jpg");
-		
-		
+		imagen = new ImageIcon("./imagenes/fondo-negro.jpg");
+
 	}
-	
+
 	public void paintComponent(Graphics g) {
-		
+
 		g.drawImage(imagen.getImage(), 0, 0, null);
-		Font font = new Font("Audiowide",Font.BOLD,50);
+		Font font = new Font("Audiowide", Font.BOLD, 50);
 		g.setFont(font);
-		g.setColor(new Color(78,105,150));
+		g.setColor(new Color(78, 105, 150));
 		g.drawString("Cargando", 210, 70);
-		
-//		Polygon polygon = new Polygon(); 
-//		polygon.addPoint(350,230);
-//		polygon.addPoint(400,305);
-//		polygon.addPoint(450,350);
-//		polygon.addPoint(500,400);
-//		g.fillPolygon(polygon);		
-		
-		
-		g.setColor(new Color(226,213,31));
+
+		// Polygon polygon = new Polygon();
+		// polygon.addPoint(350,230);
+		// polygon.addPoint(400,305);
+		// polygon.addPoint(450,350);
+		// polygon.addPoint(500,400);
+		// g.fillPolygon(polygon);
+
+		g.setColor(new Color(226, 213, 31));
 		g.fillArc(90, 160, 80, 80, 50, 275);
-		
-		g.setColor(new Color(226,31,48));
+
+		g.setColor(new Color(226, 31, 48));
 		g.drawRect(220, 200, 30, 30);
 		g.fillRect(250, 200, 30, 30);
 		g.drawRect(280, 200, 30, 30);
-		
+
 		g.fillRect(320, 200, 30, 30);
 		g.drawRect(350, 200, 30, 30);
 		g.fillRect(380, 200, 30, 30);
-		
+
 		g.drawRect(420, 200, 30, 30);
 		g.fillRect(450, 200, 30, 30);
 		g.drawRect(480, 200, 30, 30);
-		
-		
-		
+
 	}
-	
-	
-	
 
 }
